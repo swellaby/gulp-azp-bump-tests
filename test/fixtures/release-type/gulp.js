f2e6e294cf305f0ea1c36ca06e8d326aede0834e
Caleb Cartwright <caleb.cartwright@outlook.com>
2019-11-12 19:10:07 -0600

    tests: apply rename to test fixtures

--- test/fixtures/release-type/gulp.js
+++ test/fixtures/release-type/gulp.js
@@ -1,29 +1,29 @@
 'use strict';
 
 const gulp = require('gulp');
-const vstsBump = require('gulp-vsts-bump');
+const azpBump = require('gulp-azp-bump');
 const releaseBaseDir = 'release-type/';
 
 gulp.task('bump:release:default', () => {
     return gulp.src(releaseBaseDir + 'task.json', { base: './' })
-        .pipe(vstsBump())
+        .pipe(azpBump())
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:release:patch', () => {
     return gulp.src(releaseBaseDir + 'patch.json', { base: './' })
-        .pipe(vstsBump({ type: 'patch' }))
+        .pipe(azpBump({ type: 'patch' }))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:release:minor', () => {
     return gulp.src(releaseBaseDir + 'minor.json', { base: './' })
-        .pipe(vstsBump({ type: 'minor' }))
+        .pipe(azpBump({ type: 'minor' }))
         .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:release:major', () => {
     return gulp.src(releaseBaseDir + 'major.json', { base: './' })
-        .pipe(vstsBump({ type: 'major' }))
+        .pipe(azpBump({ type: 'major' }))
         .pipe(gulp.dest('./'));
 });
